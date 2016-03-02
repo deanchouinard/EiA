58c4b54954e2fb0666f710d953e6e30d95de4d5c
Dean Chouinard <deanchouinard@gmail.com>
2016-03-01 22:59:31 -0500

    key value gen server

---
+++ key_value_gen_server.ex
@@ -0,0 +1,35 @@
+defmodule KeyValueStore do
+  use GenServer
+
+  def start do
+    GenServer.start(KeyValueStore, nil)
+  end
+
+  def put(pid, key, value) do
+    GenServer.cast(pid, {:put, key, value})
+  end
+
+  def get(pid, key) do
+    GenServer.call(pid, {:get, key})
+  end
+
+  def init(_) do
+    {:ok, HashDict.new}
+  end
+
+  def handle_cast({:put, key, value}, state) do
+    {:noreply, HashDict.put(state, key, value)}
+  end
+
+  #  def handle_call({:put, key, value}, state) do
+    # {:ok, HashDict.put(state, key, value)}
+    #end
+
+  def handle_call({:get, key}, _, state) do
+    {:reply, HashDict.get(state, key), state}
+  end
+end
+
+{:ok, pid} = KeyValueStore.start
+KeyValueStore.put(pid, :akey, :avalue)
+KeyValueStore.get(pid, :akey)
