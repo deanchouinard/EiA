0879d551ba26596f6cda6d80ad60a4b7fce1ffbc
Dean Chouinard <deanchouinard@gmail.com>
2016-03-06 11:24:28 -0500

    todo cache

---
+++ todo_cache/driver.exs
@@ -0,0 +1,28 @@
+{:ok, cache} = Todo.Cache.start
+
+Todo.Cache.server_process(cache, "Bob's list")
+
+Todo.Cache.server_process(cache, "Bob's list")
+Todo.Cache.server_process(cache, "Alice's list")
+
+bobs_list = Todo.Cache.server_process(cache, "Bob's list")
+
+Todo.Server.add_entry(bobs_list, %{date: {2013, 12, 19}, title: "Dentist"})
+IO.inspect Todo.Server.entries(bobs_list, {2013, 12, 19})
+
+Todo.Cache.server_process(cache, "Alice's list") |>
+  Todo.Server.entries({2013, 12, 19}) |> IO.inspect
+
+IO.puts "verify running processes"
+{:ok, cache} = Todo.Cache.start
+
+IO.puts length(:erlang.processes)
+
+1..100_000 |>
+  Enum.each(fn(index) -> 
+    Todo.Cache.server_process(cache, "to-do list #{index}")
+  end)
+
+IO.puts length(:erlang.processes)
+
+
