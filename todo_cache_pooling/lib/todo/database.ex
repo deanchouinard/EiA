f8509e21d07d2ec06e52b94c60e5fd86b91de472
Dean Chouinard <deanchouinard@gmail.com>
2016-03-07 15:58:38 -0500

    started pooling exercise

---
+++ todo_cache_pooling/lib/todo/database.ex
@@ -0,0 +1,51 @@
+defmodule Todo.Database do
+  use GenServer
+
+  def start(db_folder) do
+    GenServer.start(__MODULE__, db_folder,
+      name: :database_server
+      )
+  end
+
+  def store(key, data) do
+    GenServer.cast(:database_server, {:store, key, data})
+  end
+
+  def get(key) do
+    Todo.DatabaseWorker.get(getworker(key), key)
+    #    GenServer.call(:database_server, {:get, key})
+  end
+
+  def init(db_folder) do
+    acc = HashDict.new
+    table = Enum.reduce([0,1,2], acc, fn(x) ->
+      {:ok, pid} = Todo.Database.Worker.start(db_folder)
+      acc = {x, pid} end)
+
+      #    File.mkdir_p(db_folder)
+      #    {:ok,  db_folder}
+  end
+
+  defp getworker(key) do
+  
+  end
+
+  def handle_cast({:store, key, data}, db_folder) do
+    file_name(db_folder, key)
+    |> File.write!(:erlang.term_to_binary(data))
+
+    {:noreply, db_folder}
+  end
+
+  def handle_call({:get, key}, _, db_folder) do
+    data = case File.read(file_name(db_folder, key)) do
+      {:ok, contents} -> :erlang.binary_to_term(contents)
+      _ -> nil
+    end
+    
+    {:reply,  data, db_folder}
+  end
+
+  defp file_name(db_folder, key), do: "#{db_folder}/#{key}"
+end
+
