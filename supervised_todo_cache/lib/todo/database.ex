b8f4095515e02cbf3c07b16f6208c8ac61dcb52d
Dean Chouinard <deanchouinard@gmail.com>
2016-03-12 11:24:23 -0500

    restart freq

---
+++ supervised_todo_cache/lib/todo/database.ex
@@ -0,0 +1,56 @@
+defmodule Todo.Database do
+  use GenServer
+
+  def start_link(db_folder) do
+    IO.puts "Starting database server."
+    GenServer.start_link(__MODULE__, db_folder,
+      name: :database_server
+      )
+  end
+
+  def store(key, data) do
+    GenServer.cast(:database_server, {:store, key, data})
+  end
+
+  def get(key) do
+    GenServer.call(:database_server, {:get, key})
+  end
+
+  def init(db_folder) do
+    acc = HashDict.new
+    table = Enum.reduce([0,1,2], acc, fn(x, acc) ->
+      {:ok, pid} = Todo.DatabaseWorker.start_link(db_folder)
+      HashDict.put(acc, x, pid) end)
+      #    IO.inspect(table)
+    {:ok, table }
+      #    File.mkdir_p(db_folder)
+      #    {:ok,  db_folder}
+  end
+
+  defp getworker(table, key) do
+    i = :erlang.phash2(key, 3)
+    HashDict.get(table, i)
+  end
+
+  def handle_cast({:store, key, data}, state) do
+    Todo.DatabaseWorker.store(getworker(state, key), key, data)
+    #    file_name(db_folder, key)
+    #|> File.write!(:erlang.term_to_binary(data))
+
+    {:noreply, state}
+  end
+
+  def handle_call({:get, key}, _, state) do
+    pid = getworker(state, key)
+    data = Todo.DatabaseWorker.get(pid, key)
+    #    data = case File.read(file_name(db_folder, key)) do
+      # {:ok, contents} -> :erlang.binary_to_term(contents)
+      # _ -> nil
+      #end
+    
+    {:reply,  data, state}
+  end
+
+  defp file_name(db_folder, key), do: "#{db_folder}/#{key}"
+end
+
