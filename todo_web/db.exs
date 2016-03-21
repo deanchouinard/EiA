c0f0338ccd5555b7b491ff7291210860aa70d231
Dean Chouinard <deanchouinard@gmail.com>
2016-03-20 22:42:05 -0400

    todo web

---
+++ todo_web/db.exs
@@ -0,0 +1,12 @@
+Todo.Supervisor.start_link
+
+bobs_list = Todo.Cache.server_process("Bob's List")
+Todo.Server.entries(bobs_list, {2013, 12, 19})
+
+IO.puts "Processes: #{length(:erlang.processes)}"
+
+Process.whereis(:database_server) |> Process.exit(:kill)
+:timer.sleep(500)
+bobs_list = Todo.Cache.server_process("Bob's List")
+IO.puts "Processes: #{length(:erlang.processes)}"
+
